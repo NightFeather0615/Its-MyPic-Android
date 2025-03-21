423ec8ed0fe0fb13d625cb3ae5a50f0eb3e9db73
NightFeather <leolee50910@gmail.com>
2025-03-22 01:22:26 +0800

    Bump v1.2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.nightfeather.its_mypic"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.2.6"
+        versionCode = 11
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
